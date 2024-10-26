e2973f65ecc026f756771203948783a5ce5809be
SamuelPacheco03 <samuel2003pacheco@gmail.com>
2024-10-26 14:22:47 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: '*', 
     credentials: true,               
   }));
 app.use('/api/v1/auth/me', authSecurity.checkAuth());
